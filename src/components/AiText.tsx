f4b32956b7b22cca753b39827859b09906ce1169
Tal Benadon <talbenadon2@gmail.com>
2025-05-01 17:14:32 +0900

    added initial components, for testing with server

---
+++ src/components/AiText.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AiText() {
+    return (
+        <div className="flex bg-white grow rounded-2xl md:h-52 mx-52">
+
+        </div>
+    )
+}
---
+++ src/components/Options.tsx
@@ -0,0 +1,8 @@
+
+export default function Options() {
+    return (
+        <div className="flex h-20 bg-white shrink-0 rounded-2xl md:h-52 mx-52 mt-6">
+
+        </div>
+    )
+}
