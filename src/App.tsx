dcd2e5b1364e2c37dc063d73c1a01ff13a4143a3
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-06 18:19:08 +0900

    resolved ai text streaming into ui, both text and options

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import AiText from "./components/AiText"
 import Options from "./components/Options/Options"
 import httpClient from "./api/httpClient"
@@ -15,10 +15,117 @@ type aiReply = {
   text: string,
   options: string[]
 }
+
 function App() {
+  const [aiFullString, setAiFullString] = useState<string>("")
+  const [adventureStory, setAdventureStory] = useState<string>("")
+  const [options, setOptions] = useState<string[]>([])
   const [aiReply, setAiReply] = useState<aiReply>()
   const [info, setInfo] = useState<adventureInfo>()
 
+  const bufferRef = useRef("")
+  const isStreaming = useRef(false)
+
+
+
+  const fetchStreamedChat = async () => {
+    const response = await fetch("http://localhost:8000/adventure/start", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: "fantasy" }),
+    });
+
+    const reader = response.body?.getReader();
+    const decoder = new TextDecoder("utf-8");
+
+
+    let currentTag = ""
+
+    const appendingFunction = (toAppend: string, currentTag: string) => {
+      switch (currentTag) {
+
+        case "text":
+          setAdventureStory(prev => prev + toAppend);
+          break;
+        case "option1":
+          setOptions((prev) => {
+            const updated = [...prev]
+            updated[0] = (updated[0] || "") + toAppend;
+            return updated
+          });
+          break;
+        case "option2":
+          setOptions((prev) => {
+            const updated = [...prev]
+            updated[1] = (updated[1] || "") + toAppend;
+            return updated
+          });
+          break;
+        case "option3":
+          setOptions((prev) => {
+            const updated = [...prev]
+            updated[2] = (updated[2] || "") + toAppend;
+            return updated
+          });
+          break;
+        default:
+          break;
+      }
+    }
+
+    while (true) {
+      const { value, done } = await reader!.read();
+      if (done) break;
+
+      const chunk = decoder.decode(value, { stream: true });
+      bufferRef.current += chunk;
+      console.log({ "currentTag": currentTag });
+
+
+      const tagStart = bufferRef.current.lastIndexOf("<");
+      const tagEnd = bufferRef.current.lastIndexOf(">");
+
+      if (tagStart !== -1) { // tag start found
+        console.log({ "tag start detected!:": bufferRef.current });
+
+        if (tagEnd !== -1) { //tag end also found
+          currentTag = bufferRef.current.slice(tagStart + 1, tagEnd)
+          console.log(currentTag);
+          bufferRef.current = bufferRef.current.slice(0, tagStart) + bufferRef.current.slice(tagEnd + 1) // cut away the tag
+          const toAppend = bufferRef.current;
+
+          appendingFunction(toAppend, currentTag)
+          bufferRef.current = "";
+        } else {
+          continue
+        }
+
+      } else {
+        const toAppend = bufferRef.current;
+        appendingFunction(toAppend, currentTag)
+        bufferRef.current = "";
+      }
+
+      // if (tagStart > tagEnd) {
+
+
+
+      //   bufferRef.current = bufferRef.current.slice(tagStart + bufferRef.current.length);
+      //   console.log(bufferRef.current);
+
+      // } else {
+
+      //   const toAppend = bufferRef.current;
+      //   setAdventureStory(prev => prev + toAppend);
+      //   bufferRef.current = "";
+      // }
+
+
+    }
+
+  };
   async function onStartClick() {
     const response = await httpClient.post<aiReply>("/adventure/start")
     console.log(response.data);
@@ -39,10 +146,10 @@ function App() {
   return (
     <main className="flex min-h-screen bg-[#ddb4ab] flex-col p-6 ">
 
-      <AiText text={aiReply?.text} />
-      {info?.status == 0 && !aiReply ? <button onClick={onStartClick}>start new cyoa</button> : ""}
+      <AiText text={adventureStory} />
+      {info?.status == 0 && !aiReply ? <button onClick={fetchStreamedChat}>start new cyoa</button> : ""}
 
-      {aiReply ? <Options options={aiReply.options} /> : ""}
+      {options.length > 0 ? <Options options={options} /> : ""}
 
     </main>
   )
--- src/components/AiText.tsx
+++ src/components/AiText.tsx
@@ -5,8 +5,10 @@ type AiTextProps = {
 export default function AiText({ text }: AiTextProps) {
     return (
         <div className="flex bg-white grow rounded-2xl md:h-52 lg:mx-52 justify-center items-center p-6">
+            <p className="whitespace-pre-wrap">
 
-            {text}
+                {text}
+            </p>
 
         </div>
     )
