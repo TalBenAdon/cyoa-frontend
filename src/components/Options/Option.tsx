99e0e0b57d3982a65b55883629728e4c77216ba4
Tal Benadon <talbenadon2@gmail.com>
2025-05-07 11:36:28 +0900

    stream parsing a bit safer, still a mess

--- src/components/Options/Option.tsx
+++ src/components/Options/Option.tsx
@@ -3,7 +3,7 @@ export type OptionProps = { text: string, handleClick: (choice: string) => void 
 
 
 export default function Option({ text, handleClick }: OptionProps) {
-    console.log({ "option": text });
+    // console.log({ "option": text });
 
     return (
         <button onClick={() => handleClick(text)} className="w-full h-full border border-black rounded-2xl cursor-pointer hover:bg-gray-200 transition duration 300 ease-in-out">
