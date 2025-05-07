52a621c69451a5f266d934815444106e7ab1ad84
Tal Benadon <talbenadon2@gmail.com>
2025-05-07 13:06:55 +0900

    saving changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AdventureProvider } from './Components/UI/AdventureContext.tsx'
+import { createBrowserRouter, redirect } from 'react-router'
+
+let router = createBrowserRouter([
+  {
+    path:"/",
+    Component : App,
+    children:[
+      {path: "venture", Component: VenturePage},
+      {index: true, loader: () => redirect("/venture")},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
