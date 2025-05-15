8278a48b3cf4324fa519a2ca6a0aed52419ea79b
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-15 23:28:57 +0900

    created processBufferChunk, parameters are passed by value, so it is returning values that should change the actual ref, it needs to be imporved, and implemented in a new streaming function

---
+++ src/utils/stream/processBufferChunk.ts
@@ -0,0 +1,50 @@
+type proccessChunkParams = {
+    chunk: string,
+    buffer: string,
+    currentTag: string,
+    appendingFn: (text: string, tag: string) => void
+}
+
+export function processBufferChunks({ chunk, buffer, currentTag, appendingFn }: proccessChunkParams
+): { newBuffer: string, newTag: string } {
+
+    buffer += chunk
+    console.log({ "currentTag": currentTag });
+    console.log({ "currentBuffer": buffer });
+
+    const tagStart = buffer.indexOf("<")
+    const tagEnd = buffer.indexOf(">", tagStart)
+
+    if (tagStart !== -1) { //tag start found
+        console.log({ "tag start detected": buffer });
+
+        if (tagEnd !== -1) { //tag end also found
+
+            const matches = buffer.match(/<[^>]+>/g)
+            if (matches && matches.length >= 2) {
+                console.log("two tags within the buffer");
+                buffer = matches[1]
+            }
+            const tagToCheck = buffer.slice(tagStart + 1, tagEnd).trim()
+            if (!tagToCheck.includes("/") && !tagToCheck.includes("\\")) {
+
+                const newTag = buffer.slice(tagStart + 1, tagEnd).trim()
+                console.log({ "tag Changed!": currentTag });
+                buffer = buffer.slice(0, tagStart) + buffer.slice(tagEnd + 1)
+                const toAppend = buffer
+                appendingFn(toAppend, newTag)
+                return { newBuffer: "", newTag }
+            } else {
+                return { newBuffer: buffer.slice(tagEnd + 1), newTag: currentTag }
+            }
+        } else {
+            const trailText = buffer.slice(0, tagEnd)
+            appendingFn(trailText, currentTag)
+            return { newBuffer: buffer.slice(tagStart), newTag: currentTag }
+        }
+    } else {
+        appendingFn(buffer, currentTag)
+        return { newBuffer: "", newTag: currentTag }
+    }
+
+}
