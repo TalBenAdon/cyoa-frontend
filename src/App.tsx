1984799821f69fd7839646bf065978085556bdaa
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-18 21:22:11 +0900

    added TODO

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router"
 
 
 function App() {
-
+  //TODO: change background color, and should probably be flex row
   return (
     <main className="flex h-screen bg-[#ddb4ab] p-6 ">
       <Outlet />
