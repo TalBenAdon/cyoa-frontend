50bd9647eab13889095244dcdf6a8f2d7735ef0a
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-20 17:12:21 +0900

    text alignment for option

--- src/Components/UI/AIChat/Option.tsx
+++ src/Components/UI/AIChat/Option.tsx
@@ -6,8 +6,10 @@ export default function Option({ text, handleClick }: OptionProps) {
     // console.log({ "option": text });
 
     return (
-        <button onClick={() => handleClick(text)} className="flex text-start px-4 py-2 p w-full h-full border border-black rounded-xl cursor-pointer hover:bg-gray-200 transition duration 300 ease-in-out">
-            {text}
+        <button onClick={() => handleClick(text)} className=" text-start px-4 py-2 p w-full h-full border border-black rounded-xl cursor-pointer hover:bg-gray-200 transition duration 300 ease-in-out">
+            <p className="flex justify-center">
+                {text}
+            </p>
         </button>
     )
 }
