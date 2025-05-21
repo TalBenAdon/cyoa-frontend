6b9344138d57447061fa275bf0ef3c2b89d8af64
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-21 22:01:27 +0900

    added DropDownMenu, gets option props, right now has internal 'revision' and 'custom action' props

--- src/Components/UI/DropDownMenu/DropDownMenu.tsx
+++ src/Components/UI/DropDownMenu/DropDownMenu.tsx
@@ -1,16 +1,50 @@
+import { useState } from 'react'
 import ArrowIcon from '../../../assets/icons/lsicon_up-outline.svg?react'
-const options = {
-    customAction: "Custom action",
-    revision: "Revision"
+import DropDownMenuList from './DropDownMenuList'
+
+const options = [
+    { id: 'customAction', label: 'Custom action' },
+    { id: 'revision', label: 'Revision' }
+]
+
+export type Option<T = string> = {
+    id: T,
+    label: string
 }
 
+
+// type DropDownMenuProps = {
+//     customAction: string,
+//     revision: string
+// }
+
 export default function DropDownMenu() {
+
+    const [isActive, setIsActive] = useState<boolean>(false)
+    const [isSelected, setIsSelected] = useState(options[0])
+
+    const handleToggle = () => setIsActive(!isActive)
+
+    const onSelect = (id: string) => {
+        const chosen = options.find((option) => option.id === id)
+        if (chosen) {
+            setIsSelected(chosen)
+
+        }
+    }
+
     return (
-        <button className="bg-button-base cursor-pointer p">
-            <p className="text-button-text-base">
-                {options.customAction}
-            </p>
-            <ArrowIcon />
-        </button>
+        <div className='relative inline-box text-base'>
+            <button className="w-[176px] flex items-center gap-2 bg-button-base cursor-pointer px-4 py-1 rounded-xl" onClick={handleToggle}>
+                <p className="text-start whitespace-nowrap text-button-text-base grow">
+                    {isSelected.label}
+                </p>
+                <ArrowIcon className={`${isActive ? "-rotate-180" : ""} duration-100 ease-linear shrink-0`} />
+            </button>
+
+            {isActive && (
+                <DropDownMenuList options={options} onSelect={onSelect} selectedId={isSelected.id} />
+            )}
+        </div>
     )
 }
