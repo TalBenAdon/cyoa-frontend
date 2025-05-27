e0914ba7ddad079e73eb7895dd3070e525ddb180
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-27 16:01:28 +0900

    added media query for font size on global css

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,3 +1,4 @@
+import EquipmentTable from "../EquipmentTable/EquipmentTable";
 
 
 export default function SideBar() {
@@ -8,7 +9,7 @@ export default function SideBar() {
         <div className="h-full side-gradient-border p-[1px] rounded-2xl">
             <div className="h-full bg-dark-main rounded-2xl">
                 <div className=" flex h-full flex-col px-3 py-4 min-w-48 rounded-2xl items-center shrink-0 side-gradient-bg">
-
+                    <EquipmentTable />
                 </div>
             </div>
         </div>
