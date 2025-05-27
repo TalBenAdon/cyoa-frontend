d1f0f8cbc82d407cdfa18301619b44eaea558704
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-27 21:17:04 +0900

    added on click and toggling the triangle down and right

--- src/Components/EquipmentTable/EquipmentTable.tsx
+++ src/Components/EquipmentTable/EquipmentTable.tsx
@@ -1,10 +1,18 @@
 import ToolTitleButton from '../ToolTitleButton/ToolTitleButton'
+import { useState } from 'react'
 import { toolIconMap } from '../../utils/ToolIconsMap'
 export default function EquipmentTable() {
+    const [isToggled, setIsToggled] = useState<boolean>(false)
+
+    const handleTitleClick = () => {
+        console.log(isToggled);
+
+        setIsToggled(!isToggled)
+    }
 
     return (
         <div>
-            <ToolTitleButton title={"Equipment"} />
+            <ToolTitleButton title={"Equipment"} isToggled={isToggled} onClick={handleTitleClick} />
 
         </div>
     )
--- src/Components/ToolTitleButton/ToolTitleButton.tsx
+++ src/Components/ToolTitleButton/ToolTitleButton.tsx
@@ -4,15 +4,16 @@ import DropTriangle from '../../assets/icons/dropDownTriangle.svg?react'
 
 type ToolTitleButtonProps = ComponentPropsWithoutRef<'button'> & {
     title: string;
+    isToggled: boolean;
 }
 
 
-export default function ToolTitleButton({ title, ...props }: ToolTitleButtonProps) {
+export default function ToolTitleButton({ title, isToggled, ...props }: ToolTitleButtonProps) {
 
 
     return (
         <button className="flex items-center gap-2 cursor-pointer" {...props}>
-            <DropTriangle className='w-3 h-3' />
+            <DropTriangle className={`w-3 h-3 ${isToggled ? 'rotate-90' : ''}`} />
             <p className="text-text-base">{title}</p>
         </button>
     )
