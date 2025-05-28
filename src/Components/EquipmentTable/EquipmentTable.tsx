a9cca16310f5a0598044659632d16e42c4d57f9a
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-28 22:49:21 +0900

    added simple style for ToolItemSlot, currently Icon recieval commented out

--- src/Components/EquipmentTable/EquipmentTable.tsx
+++ src/Components/EquipmentTable/EquipmentTable.tsx
@@ -1,6 +1,7 @@
 import ToolTitleButton from '../ToolTitleButton/ToolTitleButton'
 import { useState } from 'react'
 import { toolIconMap } from '../../utils/toolIconMap'
+import ToolItemSlot from '../UI/ToolItemColumn/ToolItemColumn'
 export default function EquipmentTable() {
     const [isToggled, setIsToggled] = useState<boolean>(false)
 
@@ -9,7 +10,9 @@ export default function EquipmentTable() {
     return (
         <div className='flex flex-col w-full'>
             <ToolTitleButton title={"Equipment"} isToggled={isToggled} onClick={handleTitleClick} />
-
+            <div className='p-1'>
+                <ToolItemSlot />
+            </div>
         </div>
     )
 }
--- src/Components/UI/ToolItemColumn/ToolItemColumn.tsx
+++ src/Components/UI/ToolItemColumn/ToolItemColumn.tsx
@@ -1,14 +1,24 @@
 import { type ItemType } from "../../../utils/toolIconMap"
-
+import RemoveIcon from '../../../assets/icons/remove.svg?react'
+import PlaceHolderIcon from "../../../assets/icons/StateWeapon.svg?react"
 type ToolItemSlotProps = {
-    Icon: React.FC<React.SVGProps<SVGSVGElement>>;
-    itemName: string | null;
+    // Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+    itemName?: string;
 }
 
-export default function ToolItemSlot() {
+export default function ToolItemSlot({ itemName }: ToolItemSlotProps) {
+    const isEmpty = !itemName
     return (
-        <div>
-
+        <div className="flex items-center text-[12px] lg:text-sm justify-between">
+            <div className="flex items-center">
+                <PlaceHolderIcon className="lg:w-[14px] lg:h-[14px]" />
+                <p className={`${isEmpty ? 'text-text-base' : ''}`}>
+                    {isEmpty ? '[empty slot]' : itemName}
+                </p>
+            </div>
+            <button>
+                <RemoveIcon className="lg:w-[12px] lg:h-[12px] hover:text-add-remove-hover" />
+            </button>
         </div>
     )
 }
