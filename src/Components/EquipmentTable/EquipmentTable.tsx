69897f1ffd1774ecadd8009a5af752b97c51866a
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-28 23:40:27 +0900

    changed E to e in equipmentslotdemo

--- src/Components/EquipmentTable/EquipmentTable.tsx
+++ src/Components/EquipmentTable/EquipmentTable.tsx
@@ -2,6 +2,8 @@ import ToolTitleButton from '../ToolTitleButton/ToolTitleButton'
 import { useState } from 'react'
 import { toolIconMap } from '../../utils/toolIconMap'
 import ToolItemSlot from '../UI/ToolItemColumn/ToolItemColumn'
+
+import { EquipmentSlotsDemo } from '../../utils/equipmentSlotsDemo'
 export default function EquipmentTable() {
     const [isToggled, setIsToggled] = useState<boolean>(false)
 
