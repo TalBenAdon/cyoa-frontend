3d2e5890d5a3a33945f423e0e1869b2f6773767f
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-29 16:03:38 +0900

    added a top border to each item, except the first in the list of the equipment table

--- src/Components/EquipmentTable/EquipmentTable.tsx
+++ src/Components/EquipmentTable/EquipmentTable.tsx
@@ -15,7 +15,7 @@ export default function EquipmentTable() {
     return (
         <div className='flex flex-col w-full'>
             <ToolTitleButton title={"Equipment"} isToggled={isToggled} onClick={handleTitleClick} />
-            <div className='flex flex-col gap-2 p-1'>
+            <div className='flex flex-col p-1 divide-y divide-gray-300'>
                 {
                     Object.entries(equipmentSlotsDemo).map(([type, item]) => {
                         const Icon = toolIconMap[type] ?? fallBackIcon
