d8b6b4e79dfe48ef314ea7fd131e9209db3f2cb4
Tal Ben Adon <talbenadon2@gmail.com>
2025-06-01 15:33:28 +0900

    saving mock data and test components, moving to overhaul of the mcp system

---
+++ src/Components/testComponents/List.tsx
@@ -0,0 +1,65 @@
+// type ToolProps = {
+//     type: 'list' | 'block';
+//     listItem: Item | null;
+//     reference: React.;
+// }
+
+
+
+
+// export function Tool(items: ToolProps) { //<- from backend
+
+//     if (type === 'list') {
+//         return <List reference={reference} />
+//     }
+
+//     return (
+
+//     )
+
+
+// }
+
+
+
+// export function ListItem({ reference }: string) {
+
+
+//     return (
+
+//     )
+// }
+
+
+// let sideBar = [
+//     {
+//         title: 'Equipment',
+//         type: 'list',
+//         selector: (state = > state.myEquipmentsstore)
+//         component: <'EquipmentComponent'>,
+//     },
+//     {
+//         title: 'Inventory',
+//         type: 'list',
+//         component: <'InventoryComponent'>,
+//     },
+//     {
+//         title: 'People',
+//         type: 'list',
+//         component: <'PeopleComponent'>,
+//     },
+
+// ]
+
+
+// export
+
+
+//     export function InventoryComponent() {
+
+//     InventoryItems = InventoryStore(state => state.inventoryItems)
+
+//     return (
+
+//     )
+// }
