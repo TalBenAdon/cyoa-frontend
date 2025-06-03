6c4f8184e5dc6acd84e65f67da8af86cd6784990
Tal Ben Adon <talbenadon2@gmail.com>
2025-06-03 12:59:59 +0900

    implemented equipment slot style to the EquipmentSlot core component

--- src/Components/SideBar/CustomCoreItems/EquipmentSlot/EquipmentSlot.tsx
+++ src/Components/SideBar/CustomCoreItems/EquipmentSlot/EquipmentSlot.tsx
@@ -1,13 +1,35 @@
-import { useToolsCoreStore } from "../../../../../stores/useToolsCoreStore"
+import { Item } from "../../../../utils/equipmentSlotsDemo"
+import FallBackIcon from '../../../../assets/icons/StateGloves.svg?react'
+import RemoveIcon from '../../../../assets/icons/remove.svg?react'
+import { toolIconMap } from "../../../../utils/toolIconMap"
+type EquipmentSlotType = {
+    item: {
+        slotName: string,
+        equippedItem: Item | null
+    }
+}
 
-export default function EquipmentSlot() {
-    const slots = useToolsCoreStore(state => state.equipmentSlots)
+export default function EquipmentSlot({ item }: EquipmentSlotType) {
+    const { slotName, equippedItem } = item
+    const iconClass = "lg:w-[12px] lg:h-[12px] hover:opacity-50 cursor-pointer"
+    const Icon = toolIconMap[slotName] ?? FallBackIcon
 
+    // const handleOnClick = () => {
+    //     console.log('slot clicked');
+    // }
 
     return (
-        <div>
-            {item} removeicon
-        </div>
+        <>
+            <button className="flex items-center gap-1">
+                <Icon className="lg:w-[14px] lg:h-[14px]" />
+                <p className={`${!equippedItem && 'text-text-base'}`}>{`${equippedItem ? equippedItem.name : '[empty slot]'}`}</p>
+            </button>
+
+            {equippedItem && <button>
+                <RemoveIcon className={iconClass} />
+            </button>}
+        </>
+
     )
 
 
